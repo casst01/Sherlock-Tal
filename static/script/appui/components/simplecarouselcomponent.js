ba82e67f5cdf991d72a439f064431953be92aee8
tom <tom@toms-linux-pc.(none)>
2013-07-04 20:31:24 +0100

    Refactor of simplecarouselfcomponent

--- static/script/appui/components/simplecarouselcomponent.js
+++ static/script/appui/components/simplecarouselcomponent.js
@@ -10,38 +10,29 @@ require.def("sampleapp/appui/components/simplecarouselcomponent",
     function (Component, DataSource, HorizontalCarousel, FriendFormatter, FriendFeed) {
         return Component.extend({
             init: function() {
-                var self, simpleFormatter, friendFeed;
-                
-                // make sure you call _super()!
+                var simpleFormatter, friendFeed;
                 this._super("simplecarouselcomponent");
-                self = this;
                 
-                // Create a new formatter and feed
                 simpleFormatter = new FriendFormatter();
                 friendFeed = new FriendFeed();
-                
-                // Create a DataSource, this uses the feed to get data and presents it to the formatter
                 this._dataSource = new DataSource(this, friendFeed, "loadData");
                 
-                // Create a new carousel with the formatter
                 this._carousel = new HorizontalCarousel("simplecarousel", simpleFormatter);
-                
-                // Add it to the component
                 this.appendChildWidget(this._carousel);
-                
-                // We want to rebind every time the component is pushed, so listen for beforerender.           
-                this.addEventListener("beforerender", function(ev) {  
-                        self._onBeforeRender(ev); 
-                    }
-                );
 
+                this._addEventListeners();
+            },
+
+            _addEventListeners: function(){
+                var self = this;
+                this.addEventListener("beforerender", function(ev) {  
+                    self._onBeforeRender(ev); 
+                });
                 this.addEventListener('aftershow', function appReady(ev) {
                     self.getCurrentApplication().ready();
                     self.removeEventListener('aftershow', appReady);
                 });
-
                 this.addEventListener('focus', self._onFocus);
-
             },
 
             _onBeforeRender: function(ev) {
