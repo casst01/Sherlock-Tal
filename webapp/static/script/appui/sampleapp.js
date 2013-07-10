679b5095374f1c6ecd7dba551f11e9d173b21efe
Tom Cass <tc@jook.net>
2013-07-10 15:48:09 +0100

    Refactored app to keep handle on rootContainer

--- webapp/static/script/appui/sampleapp.js
+++ webapp/static/script/appui/sampleapp.js
@@ -11,19 +11,15 @@ require.def('sampleapp/appui/sampleapp',
 		return Application.extend({
 			
 			init: function (appDiv, styleDir, imgDir, callback) {
-				var self = this;
-				self._super(appDiv, styleDir, imgDir, callback);
-				self._fb = new FB('407261462723217', 'http://app1.tal.com', ['user_photos']);
+				this._super(appDiv, styleDir, imgDir, callback);
+				this._fb = new FB('407261462723217', 'http://app1.tal.com', ['user_photos']);
 
-				self._setRootContainer = function() {
-					var container = new Container();
-					container.outputElement = appDiv;
-					self.setRootWidget(container);
-				}
+				this._rootContainer = new Container('app');
+				this._rootContainer.outputElement = appDiv;
 			},
 
 			run: function() {
-				this._setRootContainer();
+				this.setRootWidget(this._rootContainer);
 			},
 
 			route: function(route) {
