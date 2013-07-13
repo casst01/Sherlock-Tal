d5ea4a4d1ca9ffb47bbffa8cdb3e438166fb5c51
Tom Cass <tc@jook.net>
2013-07-13 13:22:25 +0100

    Stripped index back to show the albums component.

--- webapp/static/script/controllers/indexcontroller.js
+++ webapp/static/script/controllers/indexcontroller.js
@@ -11,23 +11,16 @@ require.def("sampleapp/controllers/indexcontroller",
             },
 
             index: function () {
-                var frame = new Frame('indexFrame');
-                frame.addComponentContainer('topContainer');
-                frame.addComponentContainer('bottomContainer');
-                this._application._rootWidget.appendChildWidget(frame);
-
-                frame.showComponent('topContainer', "sampleapp/appui/components/facebook/friendscarouselcomponent");
-
                 var self = this;
-                frame.getComponentContainer('topContainer').addEventListener('databound', function() {
-                    frame.setActiveChildWidget(frame.getComponentContainer('topContainer'));
+                var frame = new Frame('indexFrame')
+                                .addComponentContainer('albumsContainer');
+                
+                frame.getComponentContainer('albumsContainer').addEventListener('databound', function() {
+                    frame.setActiveChildWidget(frame.getComponentContainer('albumsContainer'));
                 });
-                frame.getComponentContainer('topContainer').addEventListener('select', function() {
-                    frame.showComponent('bottomContainer', "sampleapp/appui/components/facebook/albumscarouselcomponent");
-                });
-                frame.getComponentContainer('bottomContainer').addEventListener('databound', function() {
-                    frame.setActiveChildWidget(frame.getComponentContainer('bottomContainer'));
-                });
+
+                this._application._rootWidget.appendChildWidget(frame);
+                frame.showComponent('albumsContainer', "sampleapp/appui/components/facebook/albumscarouselcomponent");
             }
         });
     }
